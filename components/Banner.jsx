8a3966b14321859a92f022f3c01e9977ac3d4d07
unknown <olawanletemitayo@gmail.com>
2022-08-28 01:57:17 +0100

    added smooth transition between pages using framer motion

--- components/Banner.jsx
+++ components/Banner.jsx
@@ -1,23 +1,88 @@
+/* eslint-disable @next/next/no-img-element */
 import Image from "next/image";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "../styles/Banner.module.css";
+import { Fade } from "react-slideshow-image";
+import { motion } from "framer-motion";
+import "react-slideshow-image/dist/styles.css";
 
 const Banner = () => {
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const images = [
+    "/images/banner-bg.png",
+    "/images/banner-bg2.png",
+    "/images/banner-bg3.png",
+  ];
+
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const properties = {
+    duration: 8000,
+    transitionDuration: 2000,
+    infinite: true,
+    arrows: false,
+  };
+
+  const [imageIndex, setImageIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setImageIndex((imageIndex + 1) % images.length);
+    }, properties.duration + 1000);
+    return () => clearInterval(interval);
+  }, [images, properties, imageIndex]);
+
   return (
     <div className={styles.wrapper}>
-      <div className={styles.bg__img}>
-        {/* <Image src="/images/banner-bg.png" alt="play_img" layout="intrinsic" width={500} height={500} priority /> */}
-        {/* eslint-disable-next-line @next/next/no-img-element */}
-        <img src="/images/banner-bg.png" alt="image" />
-      </div>
+      <Fade {...properties}>
+        {images.map((image, index) => (
+          <div key={index} className={styles.bg__img}>
+            <img src={image} alt="play_img" />
+          </div>
+        ))}
+      </Fade>
+
       <div className={styles.inner}>
         <main className={styles.main}>
           <div className={styles.lhs}>
-            <h1>
-              At Kenneth’s Hybrid <br /> Foundation, we only <br /> yield the
-              best of <br /> produce
-              <span>.</span>
-            </h1>
+            <motion.div
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ duration: 1 }}
+            >
+              {imageIndex === 0 && (
+                <motion.h1
+                  initial={{ opacity: 0, translateY: -50 }}
+                  animate={{ opacity: 1, translateY: 0 }}
+                  transition={{ duration: 1 }}
+                >
+                  At Kenneth’s Hybrid <br /> Foundation, we only <br /> yield
+                  the best of <br /> produce
+                  <span>.</span>
+                </motion.h1>
+              )}
+              {imageIndex === 1 && (
+                <motion.h1
+                  initial={{ opacity: 0, translateY: 50 }}
+                  animate={{ opacity: 1, translateY: 0 }}
+                  transition={{ duration: 1 }}
+                >
+                  As we have proven over <br /> the years, we are <br />
+                  determined to <br /> revolutionize <br /> agriculture
+                  <span>.</span>
+                </motion.h1>
+              )}
+              {imageIndex === 2 && (
+                <motion.h1
+                  initial={{ opacity: 0, translateX: -50 }}
+                  animate={{ opacity: 1, translateX: 0 }}
+                  transition={{ duration: 1 }}
+                >
+                  We have completely <br /> changed the services <br /> rendered
+                  by agriculture. <br /> We offer the best of <br /> services
+                  and products<span>.</span>
+                </motion.h1>
+              )}
+            </motion.div>
             <div className={styles.learn__ctn}>
               <button type="button" className={styles.learn__more}>
                 Learn More
@@ -36,9 +101,27 @@ const Banner = () => {
             </div>
           </div>
           <div className={styles.rhs}>
-            <div className={`${styles.numbering} ${styles.active}`}>01</div>
-            <div className={styles.numbering}>02</div>
-            <div className={styles.numbering}>03</div>
+            <div
+              className={`${styles.numbering} ${
+                imageIndex === 0 && styles.active
+              }`}
+            >
+              01
+            </div>
+            <div
+              className={`${styles.numbering} ${
+                imageIndex === 1 && styles.active
+              }`}
+            >
+              02
+            </div>
+            <div
+              className={`${styles.numbering} ${
+                imageIndex === 2 && styles.active
+              }`}
+            >
+              03
+            </div>
           </div>
         </main>
       </div>
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { motion } from "framer-motion";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps, router }) {
+  return (
+    <motion.div
+      key={router.route}
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+    >
+      <Component {...pageProps} />
+    </motion.div>
+  );
 }
 
-export default MyApp
+export default MyApp;
